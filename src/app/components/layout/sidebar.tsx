abcc7fbaed7d73b1c53ed95c9f730928c8f6e1ae
fffk10 <warafffk10@gmail.com>
2024-03-16 22:13:58 +0900

    sidebarにsign outボタンの準備

--- src/app/components/layout/sidebar.tsx
+++ src/app/components/layout/sidebar.tsx
@@ -3,6 +3,8 @@
 import Link from 'next/link'
 import { RiBookLine } from 'react-icons/ri'
 import { CiSettings } from 'react-icons/ci'
+import { Button, List, ListItem } from '@yamada-ui/react'
+import { CommonButton } from '@/app/components/parts/button'
 
 type SidebarItem = {
   name: string
@@ -17,11 +19,11 @@ export const SidebarItems: SidebarItem[] = [
 
 export default function Sidebar() {
   return (
-    <aside className='hidden md:block w-64 bg-white text-gray-900 p-4 border-r '>
-      <nav>
-        <ul>
+    <aside className='hidden md:block w-52 bg-white text-gray-900 p-4 border-r '>
+      <nav className='flex flex-col h-full'>
+        <ul className='mb-auto'>
           {SidebarItems.map((item) => (
-            <li key={item.name} className='m-3'>
+            <li key={item.name} className='m-3 h-'>
               <Link
                 href={`/${item.href}`}
                 className='flex items-center text-lg'
@@ -32,6 +34,14 @@ export default function Sidebar() {
             </li>
           ))}
         </ul>
+
+        <List mt='auto'>
+          <ListItem m='0.75rem' h='1.75rem'>
+            <Button variant='unstyled' fontWeight='sm'>
+              Sign out
+            </Button>
+          </ListItem>
+        </List>
       </nav>
     </aside>
   )
